98f19b7e53defa942182d198aec42d5eb6d6db9a
PatroklosGeorgiadis <61284594+tallywoop@users.noreply.github.com>
2022-05-26 16:40:37 +0300

    Completing the Viterbi algorithm

--- 11.4/viterbi.m
+++ 11.4/viterbi.m
@@ -3,20 +3,25 @@ function [PROPABILITY_OF_SEQUENCE] = viterbi(States, Observations, Initial_state
     M = length(Observations);
     T1 = zeros(N,M);
     T2 = zeros(N,M);
+    %determine each hidden state's possibility for the first observation
     for state = 1:N
        T1(state,1) = Initial_state_matrix(state).*Emission_matrix(state, Sequence(1));
     end
+    %determine each hidden state's possibility for the rest of the observations
     for j = 2:M
+        k = argmax(T1(1:N,j-1));
+        %k is the state's most likely prior state (represented by number)
         for i = 1:N
-           k = mod(argmax(T1(i:N,j-1)), 2);
-           if k == 0
-               k = 2;
-           end
            T1(i,j) = T1(k,j-1)*Transition_matrix(k,i)*Emission_matrix(i,j);
            T2(i,j) = k;
         end
     end
     disp(T1)
+    k = argmax(T1(1:2,j));
+    for o = M:-1:1
+        PROPABILITY_OF_SEQUENCE(o) = States(k);
+        k = T2(k,o);
+    end
 end
 
 %function to find the index of the largest element in a matrix
