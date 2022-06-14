f65b2362465a0c6892f09c30d1ed38d02ba74428
PatroklosGeorgiadis <61284594+tallywoop@users.noreply.github.com>
2022-06-14 18:08:31 +0300

    Update main_ex2.m

--- 11.4/main_ex2.m
+++ 11.4/main_ex2.m
@@ -1,9 +1,11 @@
+clear
+clc
 States = ['α' 'β'];
 Observations = ['A','G','T','C'];
-Initial_state_matrix = [0.5 0.5];
-Transition_matrix = [0.9 0.1; 0.1 0.9];
-Emission_matrix = [0.4 0.4 0.1 0.1; 0.2 0.2 0.3 0.3];
+Initial_state_matrix = [0.5 ; 0.5];
+Transition_matrix = log2([0.9 0.1; 0.1 0.9]);
+Emission_matrix = log2([0.4 0.4 0.1 0.1; 0.2 0.2 0.3 0.3]);
 SequenceLetters = ['G','G','C','T'];
 Sequence = [2, 2, 4, 3];
 p = viterbi(States, Observations, Initial_state_matrix, Transition_matrix, Emission_matrix, Sequence);
-disp(p);
+disp("Η πιο πιθανή ακολουθία καταστάσεων για την ακολουθία παρατηρήσεων 'GGCT' είναι: " + p);
